0de782394c7af963869e1bab7358f802a0f7f8c6
it-samuel <samuelhappiness360@gmail.com>
2024-02-25 20:17:29 +0100

    log in screen init

--- Navigations/BottomTabs.js
+++ Navigations/BottomTabs.js
@@ -10,23 +10,41 @@ const Tabs = createBottomTabNavigator()
 
 export default function BottomTab() {
     return(
-        <Tabs.Navigator>
+        <Tabs.Navigator screenOptions={{
+            tabBarStyle:{
+                backgroundColor:"rbg(0,0,0,0.5)",
+                position:'absolute',
+                bottom:0,
+                left:0,
+                right:0,
+                shadowOpacity:4,
+                shadowRadius:4,
+                elevation:4,
+                shadowOffset:{
+                    width:0,
+                    height:-4
+                },
+                borderTopWidth:0
+            }
+        }}>
             <Tabs.Screen name="Home" component={Home} 
+
             options={{tabBarLabel:'Home', headerShown:false, tabBarLabelStyle: {color:'white'},
             tabBarIcon:({focused}) => focused ? (
-                <Entypo name="home" size={24} color="black" />
+                <Entypo name="home" size={24} color="white" />
             ) : (
-                <AntDesign name="home" size={24} color="black" />
+                <AntDesign name="home" size={24} color="white" />
             )
             }}/>
 
-<Tabs.Screen name="Profile" component={ProfileScreen} 
-            options={{tabBarLabel:'Profile', headerShown:false, tabBarLabelStyle: {color:'white'},
-            tabBarIcon:({focused}) => focused ? (
-                <Ionicons name="person-sharp" size={24} color="black" />
-            ) : (
-                <MaterialIcons name="person-outline" size={24} color="black" />
-            )
+        <Tabs.Screen name="Profile" component={ProfileScreen} 
+
+                options={{tabBarLabel:'Profile', headerShown:false, tabBarLabelStyle: {color:'white'},
+                tabBarIcon:({focused}) => focused ? (
+                    <Ionicons name="person-sharp" size={24} color="white" />
+                ) : (
+                    <MaterialIcons name="person-outline" size={24} color="white" />
+                )
             }}/>
         </Tabs.Navigator>
     )
--- Navigations/RootNav.js
+++ Navigations/RootNav.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTab from "./BottomTabs";
 import { View,Text } from "react-native";
+import LoginScreen from "../Sreens/LoginScreen";
 
 
 
@@ -11,6 +12,7 @@ export default function RootNav(){
     return(
         <NavigationContainer>
             <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
                 <Stack.Screen name="Main" component={BottomTab} options={{headerShown:false}}/>
             </Stack.Navigator>
             
